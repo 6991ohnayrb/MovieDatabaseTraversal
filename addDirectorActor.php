1beb9f857877e351055847759dc4c454bff1b708
Bryan Ho <6991ohnayrb@gmail.com>
2017-05-04 20:43:34 -0700

    add error checking for inputs

--- addDirectorActor.php
+++ addDirectorActor.php
@@ -47,27 +47,65 @@
 
 			$maxID = "";
 
+			$filled = "true";
+
+			$db_connection = mysql_connect($servername, $username, $password);
+			mysql_select_db($dbname, $db_connection);
 
 			if (isset($_POST['insert'])) {
+				$query = "select id from MaxPersonID;";
+				$rs = mysql_query($query, $db_connection);
+				$maxID = mysql_fetch_row($rs)[0];
+
 				if (isset($_POST['type'])) {
 					$type = $_POST['type'];
 				}
+				else {
+					echo "Please select a value for Actor or Director<br>";
+					$filled = "false";
+				}
+
 				$fName = $_POST['first'];
+				if ($fName == "") {
+					echo "Please enter a value for first name<br>";
+					$filled = "false";
+				}
+
 				$lName = $_POST['last'];
+				if ($lName == "") {
+					echo "Please enter a value for last name<br>";
+					$filled = "false";
+				}
+
 				if (isset($_POST['gender'])) {
 					$gender = $_POST['gender'];
 				}
-				$dob = $_POST['birth'];
-				$dod = $_POST['death'];
-
-				if ($type == "Actor") {
-					$db_connection = mysql_connect($servername, $username, $password);
-					mysql_select_db($dbname, $db_connection);
-					$query = "select id from MaxPersonID;";
-					$rs = mysql_query($query, $db_connection);
-					$maxID = mysql_fetch_row($rs)[0];
+				else {
+					echo "Please select a value for Male of Female<br>";
+					$filled = "false";
 				}
 
+				$dob = $_POST['birth'];
+				if ($dob == "") {
+					echo "Please enter a value for date of birth<br>";
+					$filled = "false";
+				}
+				else if (!(preg_match("/[0-9]{4}-[0-9]{2}-[0-9]{2}/", $dob) and strlen($dob) == 10)) {
+					echo "Please enter a valid Date of Birth in the following format: YYYY-MM-DD<br>";
+					$filled = "false";
+				}
+
+				$dod = $_POST['death'];
+				if ($dod != "" and !(preg_match("/[0-9]{4}-[0-9]{2}-[0-9]{2}/", $dod) and strlen($dod) == 10)) {
+					echo "Please enter a valid Date of Death in the following format: YYYY-MM-DD or leave blank if still alive.<br>";
+					$filled = "false";
+				}
+
+				if ($filled == "true") {
+					if ($type == "Actor") {
+						
+					}
+				}
 			}
 
 		?>
