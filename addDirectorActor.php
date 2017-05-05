42d0df8a7f09f52e37b6589595aff6c7c5274fef
Bryan Ho <6991ohnayrb@gmail.com>
2017-05-04 18:17:16 -0700

    add text fields for DOB and DOD with placeholder text

--- addDirectorActor.php
+++ addDirectorActor.php
@@ -15,12 +15,12 @@
 
 			<form action="" method="GET">
 				<strong> First Name </strong> <br>
-				<textarea name="first" cols="80" rows="1"><?php
+				<textarea name="first" cols="80" rows="1" placeholder="John"><?php
 						echo $_GET["query"];
 					?></textarea><br/>
 
 				<strong> Last Name </strong> <br>
-				<textarea name="last" cols="80" rows="1"><?php
+				<textarea name="last" cols="80" rows="1" placeholder="Doe"><?php
 						echo $_GET["query"];
 					?></textarea><br/>
 			</form>
@@ -28,6 +28,18 @@
 			<form name="testForm" id="testForm"  method="POST"  >
 				<input type="radio" name="gender" value="Male"> Male
   				<input type="radio" name="gender" value="Female"> Female<br>
+			</form>
+
+			<form action="" method="GET">
+				<strong> Date of Birth </strong> <br>
+				<textarea name="birth" cols="80" rows="1" placeholder="1970-01-01"><?php
+						echo $_GET["query"];
+					?></textarea><br/>
+
+				<strong> Date of Death </strong> (leave blank if still alive) <br>
+				<textarea name="death" cols="80" rows="1" placeholder="2070-01-01"><?php
+						echo $_GET["query"];
+					?></textarea><br/>
 			</form>
 
 		</p>
