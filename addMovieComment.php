842aa2939172c97ab05d174d77307354a762af25
Bryan Ho <6991ohnayrb@gmail.com>
2017-05-11 13:11:34 -0700

    handle inputs for addMovieComment. change rating to be drop down select

--- addMovieComment.php
+++ addMovieComment.php
@@ -20,7 +20,14 @@
 				<textarea name="mid" cols="80" rows="1" placeholder="Enter Movie ID"></textarea><br><br>
 
 				<strong> Movie Rating </strong> <br>
-				<textarea name="rating" cols="80" rows="1" placeholder="Enter Movie Rating"></textarea><br><br>
+				<select name="rating">
+					<option value="null">-----</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+				</select> <br> <br>
 
 				<strong> Comments </strong> <br>
 				<textarea name="comments" cols="80" rows="1" placeholder="Enter Comments"></textarea><br><br>
@@ -58,8 +65,44 @@
 			mysql_select_db($dbname, $db_connection);
 
 			if (isset($_POST['insert'])) {
+				$name = $_POST['name'];
+				if ($name == "") {
+					echo "Please enter a value for reviewer name<br>";
+					$filled = "false";
+				}
+				else if (strlen($name) > 20) {
+					echo "Please enter a reviewer name shorter than 20 characters<br>";
+					$filled = "false";
+				}
+
 				$date = date("Y:m:d h:m:s");
-				echo $date . "<br>";
+
+				$mid = $_POST['mid'];
+				if ($mid == "") {
+					echo "Please enter a value for movie ID<br>";
+					$filled = "false";
+				}
+				else if (!preg_match("/^[0-9]$*/", $mid)) {
+					echo "Movie ID can consist of only numbers<br>";
+					$filled = "false";
+				}
+
+				$rating = $_POST['rating'];
+				if ($rating == "null") {
+					echo "Please enter a value for rating<br>";
+					$filled = "false";
+				}
+
+				$comments = $_POST['comments'];
+				if ($comments == "") {
+					echo "Please enter a value for comments<br>";
+					$filled = "false";
+				}
+				else if (strlen($comments) > 500) {
+					echo "Please enter comments shorter than 500 characters<br>";
+					$filled = "false";
+				}
+
 			}
 		?>
 
