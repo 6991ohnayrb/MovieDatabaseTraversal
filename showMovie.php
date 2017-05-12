a5838e5418fac1e9d724b8ad9af01d067bdd4162
Bryan Ho <6991ohnayrb@gmail.com>
2017-05-12 01:05:47 -0700

    simple show movie page

---
+++ showMovie.php
@@ -0,0 +1,72 @@
+<html>
+	<head>
+		<title>
+			CS143 Project 1B
+		</title>
+	</head>
+	<body>
+		<p>
+			<form name="testForm" id="testForm"  method="GET"  >
+				<input type="submit" name="Back" value="Back" autofocus onclick="return true;"/> <br>
+			</form>
+
+			<strong> Show Movie Information </strong> <br>
+
+			<form action="" method="GET"  >
+				<strong> Search Movie ID </strong> <br>
+				<textarea name="mid" cols="80" rows="1" placeholder="Enter a Movie ID"></textarea><br><br>
+
+				<input type="submit" class="button" name="insert" value="Search Database" />
+			</form>
+		</p>
+
+		<?php
+			if(isset($_GET['Back'])) {
+				echo " 	<script type=\"text/javascript\">
+							var e = document.getElementById('testForm'); e.action='./home.php'; e.submit();
+						</script>
+					 ";
+			}
+ 		?>
+
+ 		<?php
+
+			$servername = "localhost";
+			$username = "cs143";
+			$password = "";
+			$dbname = "CS143";
+
+			$mid = "";
+			$filled = "true";
+
+			$db_connection = mysql_connect($servername, $username, $password);
+			mysql_select_db($dbname, $db_connection);
+
+			if (isset($_GET['insert'])) {
+				$mid = $_GET['mid'];
+				$query = "select * from Movie where id = $mid;";
+				echo $query."<br>";
+				$rs = mysql_query($query, $db_connection);
+
+				echo "<strong> Matching Movies </strong> <br>";
+				echo "<table><tr>";
+				for($i = 0; $i < mysql_num_fields($rs); $i++) {
+				    $field_info = mysql_fetch_field($rs, $i);
+				    echo "<th>{$field_info->name}</th>";
+				}
+
+				while($row = mysql_fetch_row($rs)) {
+				    echo "<tr>";
+				    foreach($row as $_column) {
+				        echo "<td>{$_column}</td>";
+				    }
+				    echo "</tr>";
+				}
+				echo "</table>";
+
+			}
+		?>
+
+	</body>
+</html>
+ <br>
