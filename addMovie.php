c1f6016338a18e2d926a51727ab676b3dcb2df5d
Bryan Ho <6991ohnayrb@gmail.com>
2017-05-13 00:40:37 -0700

    update left tab titles

--- addMovie.php
+++ addMovie.php
@@ -28,8 +28,8 @@
 						<li><a href="addDirectorActor.php">Add Actor or Director</a></li>
 						<li><a href="addMovie.php">Add Movie Information</a></li>
 						<li><a href="addMovieComment.php">Add Movie Comments</a></li>
-						<li><a href="addMovieActor.php">Add Movie or Actor Relation</a></li>
-						<li><a href="addMovieDirector.php">Add Movie or Director Relation</a></li>
+						<li><a href="addMovieActor.php">Add Movie Actor Relation</a></li>
+						<li><a href="addMovieDirector.php">Add Movie Director Relation</a></li>
 					</ul>
 					<ul class="nav nav-sidebar">
 						<p><strong>&nbsp;&nbsp;Search Keywords</strong></p>
